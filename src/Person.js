2197660af35aac87e97a44b21b87b27a48c6c6f4
Pikachu <himanshu.tj@gmail.com>
2019-08-28 22:56:51 +0530

    Final Touch

--- src/Person.js
+++ src/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css'
 
 const Person = ({ img, name, job}) => {
     const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
     return (
-        <div>
+        <div className="person">
             <img src={url} alt="Profile DP"/>
             <div>
                 <h4>{name}</h4>
